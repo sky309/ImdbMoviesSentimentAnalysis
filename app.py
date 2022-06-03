7a63fb1a46286a067515332fd4f83142f630d99b
Sarvesh Kumar Yadav <79853946+sky309@users.noreply.github.com>
2022-06-03 17:13:00 +0530

    add all files

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request
+import numpy as np
+import pandas as pd
+import pickle 
+import flasgger
+from flasgger import Swagger
+
+app= Flask(__name__)
+Swagger(app)
+
+mle= pickle.load(open('mle_deployment_senti_model.pkl', 'rb'))
+tfidf_vect= pickle.load(open('tfidf_imdb.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods= ['POST'])
+def predict():
+    if request.method== 'POST':
+        reviews= request.form['Reviews']
+        data= [reviews]
+        vect= tfidf_vect.transform(data).toarray()
+        my_prediction= mle.predict(vect)
+    return render_template('predict.html', prediction= my_prediction)
+
+if __name__== '__main__':
+    app.run(debug= True, Port= "5002")
